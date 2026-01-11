753e1502c34dbab331f397e7fd51a2df4db05fcf
Theo Ojochegbe <symplytheo@gmail.com>
2026-01-11 21:42:15 +0100

    feat: register routes

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -2,11 +2,7 @@ import { Stack } from "expo-router";
 
 export default function MainLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="nanny-profile" />
       <Stack.Screen name="service-booking" />
